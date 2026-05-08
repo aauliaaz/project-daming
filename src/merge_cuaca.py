6d5a84102b3bff4067b45ed0a7103e0b4b5bef54
Fatiyya Ilmi <fatiyyailmi@gmail.com>
2026-05-08 19:24:24 +0700

    fix: convert temp in us embassy

--- src/merge_cuaca.py
+++ src/merge_cuaca.py
@@ -57,6 +57,24 @@ print(df_cuaca_merged['station'].unique())
 # save
 SAVE_PATH = r'D:\fatiyya\KULIAH\TUGAS KULIAH\SEMESTER 6\DAMING\project-daming\data\data cuaca\cuaca_merged.csv'
 
+mask_us1 = df_cuaca_merged['station'] == 'us embassy 1'
+
+kolom_fahrenheit = [
+    'tempmax',
+    'tempmin',
+    'temp',
+    'feelslikemax',
+    'feelslikemin',
+    'feelslike',
+    'dew'
+]
+
+for col in kolom_fahrenheit:
+    df_cuaca_merged.loc[mask_us1, col] = (
+        (df_cuaca_merged.loc[mask_us1, col] - 32) * 5/9
+    )
+
 df_cuaca_merged.to_csv(SAVE_PATH, index=False)
 
 print('\nBERHASIL DISIMPAN')
+
